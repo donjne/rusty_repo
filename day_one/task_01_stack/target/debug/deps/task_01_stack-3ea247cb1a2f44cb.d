99f20d8e81950c6adbbff6e15c6d2ca599c42ea6
donjne <davidjrn247@gmail.com>
2025-01-02 16:58:03 +0100

    ...

--- day_one/task_01_stack/target/debug/deps/task_01_stack-3ea247cb1a2f44cb.d
+++
@@ -1,5 +0,0 @@
-/home/rusty_dave/rusty_repo/day_one/task_01_stack/target/debug/deps/libtask_01_stack-3ea247cb1a2f44cb.rmeta: src/main.rs
-
-/home/rusty_dave/rusty_repo/day_one/task_01_stack/target/debug/deps/task_01_stack-3ea247cb1a2f44cb.d: src/main.rs
-
-src/main.rs:
--- day_one/task_01_stack/target/debug/deps/task_01_stack-51d5279f16240b46.d
+++
@@ -1,5 +0,0 @@
-/home/rusty_dave/rusty_repo/day_one/task_01_stack/target/debug/deps/libtask_01_stack-51d5279f16240b46.rmeta: src/main.rs
-
-/home/rusty_dave/rusty_repo/day_one/task_01_stack/target/debug/deps/task_01_stack-51d5279f16240b46.d: src/main.rs
-
-src/main.rs:
