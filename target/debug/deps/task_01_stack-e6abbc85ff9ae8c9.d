7244e3ce9bc54f154f7e457da05a48952499ef28
donjne <davidjrn247@gmail.com>
2025-01-02 16:55:46 +0100

    added readme.md

---
+++ target/debug/deps/task_01_stack-e6abbc85ff9ae8c9.d
@@ -0,0 +1,5 @@
+/home/rusty_dave/rusty_repo/target/debug/deps/libtask_01_stack-e6abbc85ff9ae8c9.rmeta: day_one/task_01_stack/src/main.rs
+
+/home/rusty_dave/rusty_repo/target/debug/deps/task_01_stack-e6abbc85ff9ae8c9.d: day_one/task_01_stack/src/main.rs
+
+day_one/task_01_stack/src/main.rs:
---
+++ target/debug/deps/task_01_stack-ebc9290f9ab5f5bb.d
@@ -0,0 +1,5 @@
+/home/rusty_dave/rusty_repo/target/debug/deps/libtask_01_stack-ebc9290f9ab5f5bb.rmeta: day_one/task_01_stack/src/main.rs
+
+/home/rusty_dave/rusty_repo/target/debug/deps/task_01_stack-ebc9290f9ab5f5bb.d: day_one/task_01_stack/src/main.rs
+
+day_one/task_01_stack/src/main.rs:
